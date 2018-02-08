3e3bd8c7dc2eec75c3c78f80bbc7601958aa7878
Noctis Lucis <urakiny@gmail.com>
2018-02-08 13:33:34 +0800

    Modify code comments

--- detection_multithreading.py
+++ detection_multithreading.py
@@ -17,12 +17,12 @@ CWD_PATH = os.getcwd()
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
 
-# List of the strings that is used to add correct label for each box.
+# 用于为每个框添加正确标签的字符串列表
 PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-# Loading label map
+# 加载标签地图
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                             use_display_name=True)
